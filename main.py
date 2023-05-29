e65b78525b63c6c78b2b53cf1ace802843df4c57
ZingaG <trad_bar@mail.ru>
2023-05-29 21:32:20 +0300

    Task 2 completed

--- main.py
+++ main.py
@@ -1,17 +1,58 @@
 # Task 1
 
-import random
+# import random
 
-A = []
+# A = []
 
-n = int(input("Enter N = "))
+# n = int(input("Enter N = "))
 
-x = int(input("Enter X = "))
+# x = int(input("Enter X = "))
 
-for i in range(n):
-    A.append(random.randint(1,7))
+# for i in range(n):
+#     A.append(random.randint(1,7))
 
-print(*A)
+# print(*A)
 
-print(len([el for el in A if x == el]))
+# print(len([el for el in A if x == el]))
 
+# Task 2
+
+# import random
+
+# A = []
+
+# n = int(input("Enter N = "))
+
+# x = int(input("Enter X = "))
+
+# Close = 0
+
+# for i in range(n):
+#    A.append(random.randint(1,7))
+
+# print(*A)
+
+# if A[0] < x:
+#     Close = x - A[0]
+# else:
+#     Close = A[0] - x
+
+# Result = 0
+
+# for el in A:
+#     if el < x:
+#         if (x - el) < Close:
+#             Close = x - el
+#             Result = el
+#         elif x - el == 0:
+#             Result = el
+#             break
+#     else:
+#         if (el - x) < Close:
+#             Close = el - x
+#             Result = el
+#         elif x - el == 0:
+#             Result = el
+#             break
+
+# print(Result)
